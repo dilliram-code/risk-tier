32d8cfc2665d34d3c00e2b29d8bc0887fdb2cc98
dilliram-code <chaudharydr23@gmail.com>
2026-07-11 14:53:36 +0545

    load the ml model in the binary form

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel, Field, computed_field
+from typing import Literal, Optional
+import pickle 
+import pandas as pd 
+
+# import ml model
+with open("model.pkl", 'rb') as f:
+  model = pickle.load(f)
+
+app = FastAPI()
